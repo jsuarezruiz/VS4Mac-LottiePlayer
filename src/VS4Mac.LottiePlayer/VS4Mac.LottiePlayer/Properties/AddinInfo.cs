6c84097d71f74462e567b94fe6d1d802819fd8ed
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-12-27 12:25:48 +0100

    Initial code

---
+++ src/VS4Mac.LottiePlayer/VS4Mac.LottiePlayer/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "VS4Mac.LottiePlayer",
+    Namespace = "VS4Mac.LottiePlayer",
+    Version = "0.1"
+)]
+
+[assembly: AddinName("VS4Mac.LottiePlayer")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("VS4Mac.LottiePlayer")]
+[assembly: AddinAuthor("Javier Suárez Ruiz")]
